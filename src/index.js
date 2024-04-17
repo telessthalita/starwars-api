52b52fda0952cab56756f298581b21bc40fb5f0c
Thalita Teles <telessthalita@gmail.com>
2024-04-17 19:32:20 -0300

    Adiciona API Star Wars

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://telessthalita:Th110299.@starwars-api.zfv3mpj.mongodb.net/?retryWrites=true&w=majority&appName=starwars-api')
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1); // Encerra o processo com falha
+    });
+
+const Film = mongoose.model('Film', {
+    title: String,
+    description: String,
+    image_url: String,
+    trailer_url: String
+});
+
+app.get("/", async (req, res) => {
+    const films = await Film.find();
+    return res.send(films);
+});
+
+app.delete("/:id", async (req, res) => {
+    const film = await Film.findByIdAndDelete(req.params.id);
+    res.send(film);
+});
+
+app.put("/:id", async (req, res) => {
+    const film = await Film.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    return res.send(film);
+});
+
+app.post("/", async (req, res) => {
+    const film = new Film(req.body);
+    await film.save();
+    return res.send(film);
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
